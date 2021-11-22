57dd74db6cb7d37b01abfc85bff6db680ec1d9f6
BrickGoat <Brickgoat@gmail.com>
2021-11-22 15:56:25 -0500

    split runMoss function into 4 functions

--- app/lib/mossutils.js
+++ app/lib/mossutils.js
@@ -1,6 +1,10 @@
-function runMoss() {
+const glob = require("glob");
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+function getUrl(dest_txt) {
     // Reading destination directory and removing trailing route
-    let url = fs.readFileSync('./destination.txt', { encoding: 'utf-8' });
+    let url = fs.readFileSync(dest_txt, { encoding: 'utf-8' });
     url = url.substring(0, url.lastIndexOf('\\'));
 
     // Checking if the user is using tmp folder and adding C: if
@@ -12,35 +16,42 @@ function runMoss() {
 
     // Replacing all backslahes with forwardslashes
     url = url.replace(/\\/g, '/');
+    return url
+}
 
+function getFiles(url) {
     // Globbing wildcards to get all files to pass to Moss
-    glob(url + "/*/*", function (er, files) {
-        if (er) {
-            console.log(err)
+    let files = glob.sync(url + "/*/*")
+    // Surrounding file paths in quotes to avoid errors when
+    // spaces are present in file path
+    files = files.map(function (file) {
+        return '\"' + file + '\"';
+    });
+
+    files = files.join(" ");
+    return files
+}
+
+function runPerlScript(files) {
+    // Running Moss through the Strawberry Perl interpreter
+    // returns stdout
+    return execSync(`/Strawberry/perl/bin/wperl.exe ../../moss.pl ${files}`)
+}
+
+function runMoss() {
+    let promise = new Promise((res, rej) => {
+        try {
+            let url = getUrl('../routes/archive/components/destination.txt')
+            let files = getFiles(url)
+            let mossRes = runPerlScript(files)
+            res(mossRes)
+        } catch (error) {
+            rej(error)
         }
-
-        // Surrounding file paths in quotes to avoid errors when
-        // spaces are present in file path
-        files = files.map(function (file) {
-            return '\"' + file + '\"';
-        });
-
-        files = files.join(" ");
-
-        alert("This may take a minute");
-
-        // Running Moss through the Strawberry Perl interpreter
-        // and printing output to stdout
-        exec(`/Strawberry/perl/bin/wperl.exe ../../moss.pl ${files}`, (error, stdout, stderr) => {
-            if (error) {
-                alert(`exec error: ${error}`);
-                return;
-            }
-            alert(`stdout: ${stdout}`);
-        });
     })
+    return promise
 }
 
 export {
-    runMoss
+    runMoss, getUrl, getFiles, runPerlScript
 }
